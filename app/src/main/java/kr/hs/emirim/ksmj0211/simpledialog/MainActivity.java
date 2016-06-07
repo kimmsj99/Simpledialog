5a797831cd173a9b8436226509472fd71770b8a9
MinJu Kim <kimminju_99@naver.com>
2016-06-07 20:51:26 +0900

    #4 Change list Dialog

--- app/src/main/java/kr/hs/emirim/ksmj0211/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ksmj0211/simpledialog/MainActivity.java
@@ -10,12 +10,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-
+    String[] items = {"젤리빈", "킷캣", "롤리팝"};
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button but = (Button)findViewById(R.id.but_dialog);
+        but = (Button)findViewById(R.id.but_dialog);
         but.setOnClickListener(this);
     }
 
@@ -23,7 +24,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("First Dialog");
-        dialog.setMessage("This is message");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                but.setText(items[which]);
+            }
+        });
         dialog.setIcon(R.drawable.first_icon);
         dialog.setPositiveButton("확인!", new DialogInterface.OnClickListener() {
             @Override
